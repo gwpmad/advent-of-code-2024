9dcbb288e6ebbfddee98c346de9ad346bd8e202c
George Maddocks <george.maddocks@krakenflex.com>
2024-12-01 18:09:47 +0000

    Remove comment

--- day_1/main.py
+++ day_1/main.py
@@ -22,14 +22,14 @@ def solution_1(values: str) -> int:
 
 
 def solution_2(string: str) -> int:
-    length_of_line = string.find("\n")
+    step_size = string.find("\n") + 1
     state = {
         "R": defaultdict(int),
         "L": defaultdict(int),
         "total": 0,
     }
-    for i in range(0, len(string), length_of_line + 1):
-        substring = string[i : i + length_of_line + 1]
+    for i in range(0, len(string), step_size):
+        substring = string[i : i + step_size]
         left_digit, right_digit = list(map(int, substring.split()))
 
         state["L"][left_digit] += 1
@@ -72,31 +72,3 @@ test_config = {
     "expected_solution_1": 11,
     "expected_solution_2": 31,
 }
-
-# keep dict of stacks and counts for each number for both left and right, and total starting 0
-
-# { R: {}, L: {}, total: 0}
-# 3   4
-# { L: {3: 1}, R: {4: 1}, total: 0}
-# 4   3
-# do the addition both times after parsing number. [if it's more than 0 in other dict]
-# { L: {3: 1, 4: 1}, R: {4: 1}, total: 0}
-# total += ((4 * 1 * 1) - (4 * 0 * 1) = 4) = 4
-# { L: {3: 1, 4: 1}, R: {3: 1, 4: 1}, total: 4}
-# total += ((3 * 1 * 1) - (3 * 0 * 1) = 3) = 7
-# { L: {3: 1, 4: 1}, R: {3: 1, 4: 1}, total: 7}
-# 2   5
-# { L: {2: 1, 3: 1}, R: {3: 1, 4: 1, 5: 1}, total: 7}
-# 1   3
-# { L: {1: 1, 2: 1, 3: 1}, R: {3: 1, 4: 1, 5: 1}, total: 7}
-# { L: {1: 1, 2: 1, 3: 1}, R: {3: 2, 4: 1, 5: 1}, total: 7}
-# total += ((3 * 1 * 2) - (3 * 1 * 1) = 3) = 10
-# 3   9
-# { L: {1: 1, 2: 1, 3: 2}, R: {3: 2, 4: 1, 5: 1}, total: 10}
-# total += ((3 * 2 * 2) - (3 * 1 * 2) = 6) = 16
-# { L: {1: 1, 2: 1, 3: 2}, R: {3: 2, 4: 1, 5: 1, 9: 1}, total: 16}
-# 3   3
-# { L: {1: 1, 2: 1, 3: 3}, R: {3: 2, 4: 1, 5: 1, 9: 1}, total: 16}
-# total += ((3 * 3 * 2) - (3 * 2 * 2) = 6) = 22
-# { L: {1: 1, 2: 1, 3: 3}, R: {3: 3, 4: 1, 5: 1, 9: 1}, total: 22}
-# total += ((3 * 3 * 3) - (3 * 3 * 2) = 9) = 31
