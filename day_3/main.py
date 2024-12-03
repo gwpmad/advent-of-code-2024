54b2456715a824af87b2b1076b2577066d4dac57
George Maddocks <george.maddocks@krakenflex.com>
2024-12-03 10:23:07 +0000

    Day 3

---
+++ day_3/main.py
@@ -0,0 +1,34 @@
+import re
+from functools import reduce
+from math import prod
+
+from utils import open_file
+
+
+def solution(values=open_file("3")):
+    return (solution_1(values), solution_2(values))
+
+
+def solution_1(value: str):
+    ops = re.findall(r"(?<=mul\()\d+,\d+(?=\))", value)
+    return reduce(lambda acc, mul: acc + prod(map(int, mul.split(","))), ops, 0)
+
+
+def solution_2(value: str) -> int:
+    ops = re.findall(r"do\(\)|don't\(\)|(?<=mul\()\d+,\d+(?=\))", value)
+    return reduce(sum_enabled_muls, ops, {"do": True, "sum": 0})["sum"]
+
+
+def sum_enabled_muls(acc, op):
+    if op[0] == "d":
+        acc["do"] = True if op == "do()" else False
+    elif acc["do"]:
+        acc["sum"] += prod(map(int, op.split(",")))
+    return acc
+
+
+test_config = {
+    "input": "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    "expected_solution_1": 161,
+    "expected_solution_2": 48,
+}
